1e27342af4371fbad17dc63f53e455e4159939cf
David Oddoye <oddoyedavid@gmail.com>
2019-10-01 11:45:28 +0000

    intiialize student router

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 const server = require('./app/server') // import server module from app directory
+const StudentRouter = require('./app/student/router')
 
 const PORT = 5555
+
+server.use('/students', StudentRouter) // paths starting with "students" will be handled by StudentRouter
 
 // start server on port
 server.listen(PORT, () =>
