483cfa89dabebfd82e2f22f6c93d1b8743df57ba
David Oddoye <oddoyedavid@gmail.com>
2019-10-14 16:48:20 +0000

    add body-parser

--- app/server.js
+++ app/server.js
@@ -1,5 +1,8 @@
 const express = require('express') // importing express library
 const server = express() // initialising express library
+const bodyParser = require('body-parser') // initialise body-parser
+
+server.use(bodyParser.json())
 
 server.get('/', (req, res) => res.send('Hello World!')) // when user hits home page, return "Hello World!"
 
