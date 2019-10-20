84c8311bde36f07bc951040dfd8b7590f4f4cb25
Manu <kmanu7@teamcyst.com>
2019-10-20 10:41:19 +0000

    setup folders for guardian CRUD

---
+++ app/guardian/controller.js
@@ -0,0 +1,57 @@
+// connects to and retrieves data
+const { model: GuardianModel } = require('./model')
+
+/**
+ * List all guardians
+ *
+ */
+const list = async () => {
+  const guardians = await GuardianModel.find({}).lean()
+  return guardians
+}
+
+/**
+ * Get a specific guardian by their _id
+ *
+ */
+const getById = async id => {
+  const guardian = await GuardianModel.findOne({ _id: id }).lean()
+  return guardian
+}
+
+/**
+ * List guardians by name
+ *
+ */
+const listByName = async name => {
+  const guardians = await GuardianModel.find({
+    name: { $regex: new RegExp(`^${name}$`, 'i'), $options: 'i' }
+  }).lean()
+  return guardians
+}
+
+/**
+ * List guardians in a class
+ *
+ */
+const listByClass = async classId => {
+  const guardians = await GuardianModel.find({ class: classId }).lean()
+  return guardians
+}
+
+/**
+ * Create a guardian
+ *
+ */
+const create = async guardianData => {
+  const newlyCreatedStudent = await GuardianModel.create(guardianData)
+  return newlyCreatedStudent
+}
+
+module.exports = {
+  list,
+  getById,
+  listByName,
+  listByClass,
+  create,
+}
---
+++ app/guardian/handler.js
@@ -0,0 +1,95 @@
+// uses controller(s) method(s) to compose data
+
+const StudentController = require('./controller')
+
+/**
+ * List all guardians
+ *
+ */
+const list = async () => {
+  const guardians = await StudentController.list()
+  return guardians
+}
+
+/**
+ * Get a specific guardian by their _id
+ *
+ */
+const getById = async id => {
+  try {
+    // check for falsy values
+    if (!id) {
+      throw new Error('id must have a value')
+    }
+
+    const guardian = await StudentController.getById(id)
+    return guardian
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+/**
+ * List guardians by name
+ *
+ */
+const listByName = async name => {
+  try {
+    // check for falsy values
+    if (!name) {
+      throw new Error('name must have a value')
+    }
+
+    const guardians = await StudentController.listByName(id)
+    return guardians
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+/**
+ * List guardians in a class
+ *
+ */
+const listByClass = async classId => {
+  try {
+    // check for falsy values
+    if (!classId) {
+      throw new Error('class must have a value')
+    }
+
+    const guardians = await StudentController.listByClass(classId)
+    return guardians
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+/**
+ * Create a guardian
+ *
+ */
+const create = async (guardianData) => {
+  try {
+    if (!guardianData) {
+      throw new Error('Guardian data connot be null')
+    }
+
+    const newlyCreatedStudent = await StudentController.create(guardianData)
+    return newlyCreatedStudent
+  } catch(error) {
+    console.error(error)
+    throw error
+  }
+}
+
+module.exports = {
+  list,
+  getById,
+  listByName,
+  listByClass,
+  create,
+}
